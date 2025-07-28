42bad37ff688c5b6e46703adcedd80888a9747c4
Vivek Kumar <ervivekkumar677@gmail.com>
2025-07-28 19:31:38 +0530

    .env file added

--- server/db.js
+++ server/db.js
@@ -1,11 +1,14 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'medical_device_dashboard',
-    password: 'user',
-    port: 5432
+    // user: 'postgres',
+    // host: 'localhost',
+    // database: 'medical_device_dashboard',
+    // password: 'user',
+    // port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+
 });
 
 module.exports = pool;

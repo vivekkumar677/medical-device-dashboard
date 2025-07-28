08bb17a954420cd8e285a0f95900973e9735bfa8
Vivek Kumar <ervivekkumar677@gmail.com>
2025-07-29 02:02:00 +0530

    .env updated

--- server/db.js
+++ server/db.js
@@ -1,11 +1,17 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'medical_device_dashboard',
-    password: 'user',
-    port: 5432
+    // user: 'postgres',
+    // host: 'localhost',
+    // database: 'medical_device_dashboard',
+    // password: 'user',
+    // port: 5432
+
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 
 });
 
